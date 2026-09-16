4617c8e365fb64dea2ea73b24af21a6381b65bf1
xiongand <xiongand@univ-rouen.fr>
2026-09-16 13:17:49 +0200

    Ajout de l'etape 1 : script genmv_1.bat

---
+++ script/genmv_1.bat
@@ -0,0 +1,20 @@
+@echo off
+set NOM_VM=Debian1
+
+echo Creation de la VM %NOM_VM%...
+VBoxManage createvm --name %NOM_VM% --ostype Debian_64 --register
+
+VBoxManage modifyvm %NOM_VM% --memory 4096 --nic1 nat
+
+:: On spécifie le nom du fichier VDI (VirtualBox le stockera dans le dossier de la VM)
+VBoxManage createmedium disk --filename "%NOM_VM%.vdi" --size 65536
+
+VBoxManage storagectl %NOM_VM% --name "SATA" --add sata --controller IntelAhci
+VBoxManage storageattach %NOM_VM% --storagectl "SATA" --port 0 --device 0 --type hdd --medium "%NOM_VM%.vdi"
+
+echo Machine creee avec succes dans le dossier VirtualBox par defaut.
+pause
+
+echo Suppression de la VM %NOM_VM%...
+VBoxManage unregistervm %NOM_VM% --delete
+echo Machine supprimee.
